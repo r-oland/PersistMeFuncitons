06472e8c03f0a204840e6c8cfe371ca35b9b248e
Roland Branten <roland.branten@gmail.com>
2020-06-18 22:19:53 +0200

    added first functions

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,39 @@
 const functions = require("firebase-functions");
+var admin = require("firebase-admin");
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: "https://persistme-d937f.firebaseio.comm",
+});
+
+exports.deleteUser = functions.auth.user().onDelete(async (user) => {
+  const snapshot = await admin
+    .firestore()
+    .collection("users")
+    .where("userId", "==", user.uid)
+    .get();
+
+  snapshot.forEach((doc) => {
+    doc.ref.delete();
+  });
+});
+
+exports.createUserDoc = functions.https.onCall(async (username, context) => {
+  try {
+    const user = await admin
+      .firestore()
+      .collection("users")
+      .doc(username)
+      .get();
+
+    if (!user.exists) {
+      admin.firestore().collection("users").doc(username).set({
+        userId: context.auth.uid,
+        username: username,
+        subscriptionDate: new Date(),
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
